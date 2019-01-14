b97598bb6ef1ccf96f775a6642ad3fec4f0a392b
S1lentem <tema.yarmosh@gmail.com>
2019-01-14 03:00:16 +0300

    Added menu, fix keyboard and refactoring code

--- RSSReader/MainPage.xaml.cs
+++ RSSReader/MainPage.xaml.cs
@@ -1,55 +1,37 @@
-﻿using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-
-using RSSController;
-using System.Text;
+﻿using System;
 using System.Collections.Generic;
-using Windows.UI.Popups;
-using System;
-using System.Net.Http;
-using System.Xml;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
 
-// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace RSSReader
 {
-
     /// <summary>
     /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        private IEnumerable<RSSFeed> listRssFeeds;
-
         public MainPage()
         {
             this.InitializeComponent();
-
-            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-            Encoding.GetEncoding("windows-1254");
         }
 
-        private async void LoadButtonClick(object sender, RoutedEventArgs e)
+        private void SelectItemFromMenu(object sender, SelectionChangedEventArgs e)
         {
-            var link = RssFeedsLink.Text.CreateLink();
-
-            RSSFeedController feedController = new RSSFeedController(link);
-
-            try
-            {
-                var feeds = await feedController.GetFeeds();
-                listRssFeeds = feeds;
-        
-            }
-            catch (Exception ex) when (ex is  UriFormatException || ex is HttpRequestException || ex is XmlException)
-            {
-                var dialog = new MessageDialog("Invalid link");
-                dialog.ShowAsync();
-            }
-            finally
-            {
-                RssFeeds.ItemsSource = listRssFeeds;
-            }
+            
         }
+
+        private void ShowMenuClick(object sender, RoutedEventArgs e) => menu.IsPaneOpen = !menu.IsPaneOpen;
     }
 }
---
+++ RSSReader/NewsPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+using RSSController;
+using System.Text;
+using System.Collections.Generic;
+using Windows.UI.Popups;
+using System;
+using System.Net.Http;
+using System.Xml;
+using Windows.System;
+using Windows.UI.Xaml.Input;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
+
+namespace RSSReader
+{
+
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class NewsPage : Page
+    {
+        private IEnumerable<RSSFeed> listRssFeeds;
+
+        public NewsPage()
+        {
+            this.InitializeComponent();
+
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            Encoding.GetEncoding("windows-1254");
+        }
+
+        private async void RssSourceKeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == VirtualKey.Enter)
+            {
+                var link = RssFeedsLink.Text.CreateLink();
+
+                // Removes keyboard when pressed enter
+                Windows.ApplicationModel.Core.CoreApplication.GetCurrentView().CoreWindow.IsInputEnabled = false;
+                Windows.ApplicationModel.Core.CoreApplication.GetCurrentView().CoreWindow.IsInputEnabled = true;
+
+                RSSFeedController feedController = new RSSFeedController(link);
+
+                try
+                {
+                    var feeds = await feedController.GetFeeds();
+                    listRssFeeds = feeds;
+
+                }
+                catch (Exception ex) when (ex is UriFormatException || ex is HttpRequestException || ex is XmlException)
+                {
+                    var dialog = new MessageDialog("Invalid link");
+                    dialog.ShowAsync();
+                }
+                finally
+                {
+                    RssFeeds.ItemsSource = listRssFeeds;
+                }
+            }
+        }
+    }
+}
