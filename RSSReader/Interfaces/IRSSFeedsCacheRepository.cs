6bc7ab75197cf328c457a3ff4b7eb572b38bd4ab
S1lentem <tema.yarmosh@gmail.com>
2019-01-21 02:40:51 +0300

    Added repository for rss feeds in cache

---
+++ RSSReader/Interfaces/IRSSFeedsCacheRepository.cs
@@ -0,0 +1,11 @@
+﻿using RSSController;
+using System.Collections.Generic;
+
+namespace RSSReader.Interfaces
+{
+    interface IRSSFeedsCacheRepository
+    {
+        void PushInCache(params IEnumerable<RSSFeedItem>[] items);
+        IEnumerable<RSSFeedItem> GetFromCache();
+    }
+}
